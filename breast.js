d00db880aa20d92141788de09784b887a2e11cb5
Muhammed Adnan <147593005+muhammedadnanv@users.noreply.github.com>
2024-03-02 19:47:52 +0530

    Update breast.js

--- breast.js
+++ breast.js
@@ -1,17 +1,1 @@
-// Stocks TRADE:
-{
-    "conditions": [
-        12,
-        37
-    ],
-    "exchange": 4,
-    "id": "187115",
-    "participant_timestamp": 1679702398739000000,
-    "price": 160.23,
-    "sequence_number": 5739937,
-    "sip_timestamp": 1679702398741170000,
-    "size": 1,
-    "tape": 3,
-    "trf_id": 12,
-    "trf_timestamp": 1679702398741113300
-}
+
